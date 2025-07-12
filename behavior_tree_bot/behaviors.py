0f697134b89506337964bb17190fbf0b4b3d869f
hojounyoon <140112030+hojounyoon@users.noreply.github.com>
2025-07-12 12:24:34 +0800

    Update behaviors.py

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -39,3 +39,20 @@ def spread_to_weakest_neutral_planet(state):
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
+
+def reinforce_weakest_my_planet(state):
+    # If no planets or only one planet, skip
+    my_planets = state.my_planets()
+    if len(my_planets) < 2 or len(state.my_fleets()) >= 1:
+        return False
+
+    # Find weakest and strongest planets
+    weakest = min(my_planets, key=lambda p: p.num_ships, default=None)
+    strongest = max(my_planets, key=lambda p: p.num_ships, default=None)
+
+    if weakest.ID == strongest.ID or strongest.num_ships < 20:
+        return False  # nothing to reinforce or too few ships to help
+
+    # Send 1/3 of ships to weakest planet
+    num_to_send = strongest.num_ships // 3
+    return issue_order(state, strongest.ID, weakest.ID, num_to_send)
